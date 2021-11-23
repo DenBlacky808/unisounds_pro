b6d3a2653de02fc79ea2ee13848a77f6d735fe02
Denis <247300@gmail.com>
2021-11-23 13:58:04 +0300

    add pagination

--- unisounds_pro/settings.py
+++ unisounds_pro/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = int(os.environ.get("DEBUG", default=0))
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 ALLOWED_HOSTS = ['5.63.154.57', 'unisounds.pro']
